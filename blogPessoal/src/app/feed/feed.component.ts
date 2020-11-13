14d5c89bd0b96977764c4f8f080c3f362043f1da
Gustavo Godoy <gustavocoliver@gmail.com>
2020-11-13 15:50:13 -0300

    Consumo API e CRUD completo

--- blogPessoal/src/app/feed/feed.component.ts
+++ blogPessoal/src/app/feed/feed.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 import { Postagem } from '../model/Postagem';
 import { Tema } from '../model/Tema';
 import { AlertasService } from '../service/alertas.service';
@@ -33,10 +34,10 @@ export class FeedComponent implements OnInit {
      ) { }
 
   ngOnInit(){
-    
-    let token = localStorage.getItem('token')
 
-    if(token == null){
+    let token = environment.token
+
+    if(token == ''){
       this.router.navigate(['/login'])
       this.alert.showAlertInfo('Faça o login para começar!')
     }
